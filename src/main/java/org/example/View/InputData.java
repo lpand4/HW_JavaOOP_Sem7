5a945bbead08db7bffc74854ce4156ab59100b97
Vladislav <vlad-pugovkin2@tandex.ru>
2023-03-28 20:28:13 +0300

    Add some check on input

--- src/main/java/org/example/View/InputData.java
+++ src/main/java/org/example/View/InputData.java
@@ -12,25 +12,31 @@ public class InputData {
     }
 
     public ComplexNumber requestNum(String msg) {
-        Scanner in = new Scanner(System.in);
-        System.out.println(msg);
-        String userNumber = in.nextLine();
-        String[] partsOfNumber = userNumber.split(" ");
-        if (partsOfNumber.length == 3) {
-            if (partsOfNumber[1].equals("-"))
-                return new ComplexNumber(Double.parseDouble(partsOfNumber[0]),
-                        Double.parseDouble(partsOfNumber[2].toLowerCase().replace("i","")) * -1);
-            else
-                return new ComplexNumber(Double.parseDouble(partsOfNumber[0]),
-                        Double.parseDouble(partsOfNumber[2].toLowerCase().replace("i","")));
-        } else if (partsOfNumber[0].equals("i"))
-            return new ComplexNumber(0,1);
-        else if (partsOfNumber[0].equals("-i"))
-            return new ComplexNumber(0,-1);
-        else if (partsOfNumber[0].contains("i"))
-            return new ComplexNumber(0, Double.parseDouble(partsOfNumber[0].replace("i","")));
-        else
-            return new ComplexNumber(Double.parseDouble(partsOfNumber[0]), 0);
+        while (true){
+            try {
+                Scanner in = new Scanner(System.in);
+                System.out.println(msg);
+                String userNumber = in.nextLine();
+                String[] partsOfNumber = userNumber.split(" ");
+                if (partsOfNumber.length == 3) {
+                    if (partsOfNumber[1].equals("-"))
+                        return new ComplexNumber(Double.parseDouble(partsOfNumber[0]),
+                                Double.parseDouble(partsOfNumber[2].toLowerCase().replace("i", "")) * -1);
+                    else
+                        return new ComplexNumber(Double.parseDouble(partsOfNumber[0]),
+                                Double.parseDouble(partsOfNumber[2].toLowerCase().replace("i", "")));
+                } else if (partsOfNumber[0].equals("i"))
+                    return new ComplexNumber(0, 1);
+                else if (partsOfNumber[0].equals("-i"))
+                    return new ComplexNumber(0, -1);
+                else if (partsOfNumber[0].contains("i"))
+                    return new ComplexNumber(0, Double.parseDouble(partsOfNumber[0].replace("i", "")));
+                else
+                    return new ComplexNumber(Double.parseDouble(partsOfNumber[0]), 0);
+            }catch (NumberFormatException e){
+                System.out.println("Неверный ввод числа");
+            }
+        }
     }
     public String requestMathSign(String msg) {
         Scanner in = new Scanner(System.in);
